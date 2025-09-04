d5ccd2ed2c5df6be24dbdfa775dd3adc582ea7ef
ItsVaishnav <vaishnavghadgepc99@gmail.com>
2025-09-04 12:00:36 +0530

    sum of digits

--- sum_of_digits.java
+++ sum_of_digits.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
 
-public class rever_num {
+public class sum_of_digits {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a number: ");
         int num = sc.nextInt();
 
-        int reversed = 0;
+        int sum = 0;
 
         while (num != 0) {
-            int digit = num % 10; 
-            reversed = reversed * 10 + digit; 
-            num = num / 10; 
+            int digit = num % 10;  
+            sum += digit;          
+            num = num / 10;        
         }
 
-        System.out.println("Reversed Number: " + reversed);
+        System.out.println("Sum of digits: " + sum);
+
     }
 }
